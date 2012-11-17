d97c278d62580a73bbb9afbcc3c130357090aecf
Michael Bosworth <mtbosworth@gmail.com>
2012-11-17 14:23:32 -0600

    node environment detection

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ var Stack = require('stack'),
     Creationix = require('creationix'),
     Http = require('http');
 
+console.log(process.env.NODE_ENV);
 Http.createServer(Stack(
   Creationix.log(),
   require('wheat')( __dirname + "/.." )
-)).listen(process.env == 'production' ? process.env.PORT : 8080);
+)).listen(process.env.NODE_ENV == 'production' ? process.env.PORT : 8080);
