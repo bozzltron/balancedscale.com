70540e0f6d2244c9f056df38d955cc0b860a77db
Michael Bosworth <mtbosworth@gmail.com>
2013-06-02 07:37:20 -0400

    contact form

--- app.js
+++ app.js
@@ -30,6 +30,27 @@ hbs.registerHelper('numPages', function() {
 });
 
 
+// all environments
+app.use(express.bodyParser());
+app.use(express.compress());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.enable('trust proxy');
+app.use( poet.middleware() );
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view options', {layout:true});
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.engine('html', hbs.express3({
+  partialsDir: __dirname + '/views',
+  defaultLayout: __dirname + '/views/layout.html'
+}));
+app.set('view engine', 'html');
+
 // poet
 poet.set({
   posts: './posts/',  // Directory of posts
@@ -47,78 +68,21 @@ poet.set({
 
 app.set('poet', poet);
 
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.engine('html', hbs.express3({
-  partialsDir: __dirname + '/views',
-  defaultLayout: __dirname + '/views/layout.html'
-}));
-app.set('view engine', 'html');
-app.use(express.compress());
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.enable('trust proxy');
-app.use( poet.middleware() );
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view options', {layout:true});
-
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 // Routeage
-app.get('/', routes.index);
-app.get('/login', routes.login);
-app.get('/about', routes.about);
-app.get('/blog/:yearmonth/:post', function ( req, res ) {
-    res.redirect('/blog/' + req.params.post); 
-});
-
-app.get( '/page/:page', function ( req, res ) {
-  var page = req.params.page,
-    lastPost = page * 10
-
-    var pagination = '<div class="pagination"><ul>';
-    var i = 1;
-    while ( i <= res.app.locals.getPageCount() ) {
-        if (res.app.locals.page == i) {
-            pagination += '<li class="active"><a href="' + res.app.locals.pageUrl(i) + '">' + i + '</a></li>';
-        }
-        else {
-          pagination += '<li><a href="' + res.app.locals.pageUrl(i) + '">' + i + '</a></li>';
-        }
-        i++;
-    }
-    pagination += '</ul></div>';
-
-  res.render( 'page', {
-    posts : res.app.locals.getPosts( lastPost - 10, lastPost ),
-    page : page,
-    pagination: pagination
-  });
-});
-
-app.get( '/tags/:tag', function ( req, res ) {
-  var taggedPosts = res.app.locals.postsWithTag( req.params.tag );
-  if ( taggedPosts.length ) {
-    res.render( 'tag', {
-      posts : taggedPosts,
-      tag : req.params.tag
-    });
-  }
-});
-
-app.get( '/tags' , function( req, res ) {
-  res.render( 'tags' , { tagList : res.app.locals.tagList } );
-});
+app.post( '/contact', routes.sendEmail );
+app.get( '/', routes.index );
+app.get( '/login', routes.login );
+app.get( '/about', routes.about );
+app.get( '/contact', routes.contact );
+app.get( '/blog/:yearmonth/:post', routes.legacy );
+app.get( '/page/:page', routes.page );
+app.get( '/tags/:tag', routes.tag );
+app.get( '/tags' , routes.tags );
 
 function setupRss ( core ) {
   app.get('/rss', function ( req, res ) {
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,7 @@
 /*
  * Main routes
  */
-// var posts = require('../import.json');
-
-// posts.forEach(function(post){
-//     post.url = post.link.replace('http://balancedscale.com','');
-// });
+var nodemailer = require('nodemailer');
 
 exports.index = function(req, res){
     var posts = res.app.locals.getPosts(0,10);
@@ -47,4 +43,76 @@ exports.login = function(req, res){
 
 exports.about = function(req, res){
     res.render('about');
+};
+
+exports.tags = function( req, res ) {
+  res.render( 'tags' , { tagList : res.app.locals.tagList } );
+};
+
+exports.tag = function ( req, res ) {
+  var taggedPosts = res.app.locals.postsWithTag( req.params.tag );
+  if ( taggedPosts.length ) {
+    res.render( 'tag', {
+      posts : taggedPosts,
+      tag : req.params.tag
+    });
+  }
+};
+
+exports.legacy = function ( req, res ) {
+    res.redirect('/blog/' + req.params.post); 
+};
+
+exports.page = function ( req, res ) {
+  var page = req.params.page,
+    lastPost = page * 10
+
+    var pagination = '<div class="pagination"><ul>';
+    var i = 1;
+    while ( i <= res.app.locals.getPageCount() ) {
+        if (res.app.locals.page == i) {
+            pagination += '<li class="active"><a href="' + res.app.locals.pageUrl(i) + '">' + i + '</a></li>';
+        }
+        else {
+          pagination += '<li><a href="' + res.app.locals.pageUrl(i) + '">' + i + '</a></li>';
+        }
+        i++;
+    }
+    pagination += '</ul></div>';
+
+  res.render( 'page', {
+    posts : res.app.locals.getPosts( lastPost - 10, lastPost ),
+    page : page,
+    pagination: pagination
+  });
+  
+};
+
+exports.contact = function(req, res){
+  res.render('contact', { title: 'Contact' });
+};	
+
+exports.sendEmail = function(req, res) {
+
+    if(typeof(req.body.from) != undefined && req.body.subject && req.body.message) {
+  
+        var SendGrid = require('sendgrid').SendGrid;
+        var sendgrid = new SendGrid('app15875802@heroku.com', 'dclxll9e');
+        sendgrid.send({
+          to: 'mtbosworth@gmail.com',
+          from: req.body.from,
+          subject: 'Balancedscale.com : ' + req.body.subject,
+          text: req.body.message
+        }, function(success, message) {
+          if (!success) {
+            res.render('contact', {messages:[{type:"error", message:"There Was A Problem.  Try Again."}]});
+            console.log(message);
+          } else {
+            res.render('contact', {messages:[{type:"success", message:"Your Message Was Sent.  Thank You!"}]});  
+          }
+        });
+
+    } else {
+        res.render('contact', {messages:[{type:"error", message:"Please fill out all of the fields!"}]});
+    }
 };
